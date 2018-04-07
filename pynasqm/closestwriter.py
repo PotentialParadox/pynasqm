8586889ca3c75c4381b6ece3ebbda41115dd68c1
Dustin Tracy <dtracy.uf@gmail.com>
2018-04-07 10:41:44 -0400

    created closestwriter

---
+++ pynasqm/closestwriter.py
@@ -0,0 +1,45 @@
+class ClosestWriter:
+
+    def __init__(self, trajins, number_nearest):
+        self.trajins = self._list(trajins)
+        self.number_nearest = number_nearest
+        self.prmtop = "m1.prmtop"
+        self.solvent_mask = "!:1"
+        self.focus_mask = ":1"
+        self.trajouts = self._default_trajout()
+
+    @staticmethod
+    def _list(trajins):
+        if isinstance(trajins, str):
+            return [trajins]
+        return trajins
+
+    def _default_trajout(self):
+        closest_outputs = []
+        for trajectory in range(1, self._number_trajectories() + 1):
+            closest_outputs.append("closest_{}.txt".format(trajectory))
+        return closest_outputs
+
+    def _number_trajectories(self):
+        return len(self.trajins)
+
+    def write(self):
+        for i in range(self._number_trajectories()):
+            script_filename = "closest_{}.traj".format(i+1)
+            script = self._closest_script(self.trajins[i], self.trajouts[i])
+            open(script_filename, 'w').write(script)
+
+    def _closest_script(self, trajin, trajout):
+        '''
+        Creates a script to identify near solvents to later
+        be included in the QM calculation
+        '''
+        script = "parm {}\n".format(self.prmtop)
+        script += "solvent !:1\n"
+        script += "trajin {}\n".format(trajin)
+        script += "closest {} {} closestout {}\n".format(self.number_nearest,
+                                                                     self.focus_mask,
+                                                                     trajout)
+        script += "run\n" \
+                  "quit\n"
+        return script
---
+++ tests/test_closestwriter.py
@@ -0,0 +1,39 @@
+'''
+Units tests for the cpptraj wrappers for nasqm
+'''
+import os
+import pytest
+import numpy as np
+from pynasqm.closestwriter import ClosestWriter
+import pynasqm.userinput as nasqm_user_input
+import pynasqm.inputceon as inputceon
+
+def setup_module(module):
+    '''
+    Switch to test directory
+    '''
+    os.chdir("tests")
+
+def teardown_module(module):
+    '''
+    Return to main directory
+    '''
+    os.chdir("..")
+
+def test_write_closest_traj():
+    trajin = "ground_snap.1"
+    number_nearest = 3
+    closests = ClosestWriter(trajin, number_nearest)
+    closests.write()
+    result = open("closest_1.traj", 'r').read()
+    answer = open("closest_1_test.traj", 'r').read()
+    assert result == answer
+
+def test_write_closest_traj2():
+    trajin = ["ground_snap.1", "ground_snap.2"]
+    number_nearest = 3
+    closests = ClosestWriter(trajin, number_nearest)
+    closests.write()
+    result = open("closest_2.traj", 'r').read()
+    answer = open("closest_2_test.traj", 'r').read()
+    assert result == answer
