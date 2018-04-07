e73498ec795fa2e5367dd74cd777c779bf180c68
Dustin Tracy <dtracy.uf@gmail.com>
2018-04-07 15:55:30 -0400

    renamed outputs to closest_outputs

--- pynasqm/solventmaskupdater.py
+++ pynasqm/solventmaskupdater.py
@@ -3,15 +3,15 @@ from pynasqm.closestrunner import ClosestRunner
 
 class SolventMaskUpdater:
 
-    def __init__(self, input_ceons, user_input, outputs):
+    def __init__(self, input_ceons, user_input, closest_outputs):
         self._input_ceons = input_ceons
         self._user_input = user_input
-        self._outputs = outputs
+        self._closest_outputs = closest_outputs
         self._trajins = self._default_trajins()
         self._masks = None
 
     def update_masks(self):
-        self._create_masks(self._outputs)
+        self._create_masks(self._closest_outputs)
         self._set_masks_in_input()
 
     def _default_trajins(self):
@@ -36,9 +36,6 @@ class SolventMaskUpdater:
         mask += "'"
         return mask
 
-
-    def _number_nearest_solvents(self):
-        return self._user_input.number_nearest_solvents
 
     def _number_trajectories(self):
         return len(self._input_ceons)
