7bca2ba7d8e89a26a88287dcea1518e5a4392ac7
Dustin Tracy <dtracy.uf@gmail.com>
2018-04-16 10:16:49 -0400

    reverted back to trusted versions of hist_spectra

--- scripts/hist_spectra_lifetime.py
+++ scripts/hist_spectra_lifetime.py
@@ -1,10 +1,9 @@
 import argparse
-import sys
-from pynasqm.calculatespectra import calculate_spectra, average_spectra, np_to_string
+from pynasqm.calculatespectra import calculate_spectra
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_file", "-i", help="The input file", default=None)
-parser.add_argument("--output_file", "-o", help="The output file", default=None)
+parser.add_argument("--input", "-i", help="The input file", default=None)
+parser.add_argument("--output", "-o", help="The output file", default=None)
 parser.add_argument("--number_states", help="The number of excited states you want to extract",
                     default=1, type=int)
 parser.add_argument("--number_gauss", help="The number of gaussians", default=100, type=int)
@@ -15,10 +14,5 @@ parser.add_argument("--xmin", help="The minimum energy in eV", default=2.0, type
 parser.add_argument("--xmax", help="The maximum energy in eV", default=3.0, type=float)
 args = parser.parse_args()
 
-input_file = args.input_file
-output_file = args.output_file
-spectra = calculate_spectra(args.number_states, args.number_gauss, args.fwhm, args.number_bins,
-                            args.xmin, args.xmax, input_file, output_file)
-
-spectra_string = np_to_string(spectra)
-open(output_file, 'w').write(spectra_string)
+calculate_spectra(args.number_states, args.number_gauss, args.fwhm, args.number_bins,
+args.xmin, args.xmax, args.input, args.output)
