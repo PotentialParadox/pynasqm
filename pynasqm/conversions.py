72592ece82d33af59280c4bfb57157e2b52f00f6
Dustin Tracy <dtracy.uf@gmail.com>
2018-06-08 13:18:45 -0400

    fixed type-o's with conversions

--- pynasqm/conversions.py
+++ pynasqm/conversions.py
@@ -1,9 +1,10 @@
 atomic_to_ps = 2.41888E-5
-ps_to_atomic = 1 / atomic_to_ps
+ps_to_atomic = 1.0 / atomic_to_ps
 atomic_to_angstrom = 0.529177249
-angstrom_to_atomic = 1 / atomic_to_angstrom
+angstrom_to_atomic = 1.0 / atomic_to_angstrom
 amber_to_angstrom_ps = 20.455
+angstrom_ps_to_amber = 1.0 / amber_to_angstrom_ps
 angstrom_ps_to_atomic =  angstrom_to_atomic / ps_to_atomic
 amber_to_atomic = amber_to_angstrom_ps * angstrom_ps_to_atomic
-atomic_to_amber = 1 / amber_to_atomic
-atomic_to_angstrom_ps = 1 / angstrom_ps_to_atomic
+atomic_to_amber = 1.0 / amber_to_atomic
+atomic_to_angstrom_ps = 1.0 / angstrom_ps_to_atomic
