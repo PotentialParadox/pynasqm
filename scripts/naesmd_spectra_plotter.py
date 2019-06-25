e38b97fc7cbc68156a16814663e95d6d4e323a6d
Dustin Tracy <dtracy.uf@gmail.com>
2019-06-25 13:34:07 -0400

    Use seaborne theme

--- scripts/naesmd_spectra_plotter.py
+++ scripts/naesmd_spectra_plotter.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pynasqm.utils
 from scipy.interpolate import spline
 
@@ -8,12 +9,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--title", help="The title of the graph", default="Title")
 parser.add_argument("--number_states", help="The number of states you want to include.",
                     default=1, type=int)
-parser.add_argument("--labels", "-l", help="labels of the data", default=[1], nargs="+")
+parser.add_argument("--labels", "-l", help="labels of the data", default=[1,2,3,4,5,6,7,8,9,10,11,
+                                                                          12,13,14,15,16,17,18,19], nargs="+")
+parser.add_argument("--ylabel", help="labels for you y axis", default="")
 parser.add_argument("--inputfile", "-i", help="The input file", default=None)
+parser.add_argument("--outputfile", "-o", help="The output file", default=None)
+parser.add_argument("--letter", help="The letter in the paper", default="")
 parser.add_argument("--comparison", "-c",
                     help="Are you comparing spectra?",
                     default="False")
-parser.add_argument("--x_units", "-x", help="0-Ev or 1-nm", default=1, type=int)
+parser.add_argument("--x_units", "-x", help="0-Ev or 1-nm", default=0, type=int)
 parser.add_argument("--range", "-r", help="Provide two numbers,"\
                     "the minimum and maximum values in the units you prefer",
                     default=[0.0, 100.0], nargs="+", type=float)
@@ -22,20 +27,51 @@ args = parser.parse_args()
 
 args.comparison = pynasqm.utils.str2bool(args.comparison)
 
-color_code = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b', 'g', (0.1, 0.2, 0.5)]
-
 data = np.loadtxt(args.inputfile)
 
 state_intensity_index_start = 2
 state_intensity_index_end = 2 + args.number_states
-x = data[:, args.x_units]
-ys = data[:, state_intensity_index_start:state_intensity_index_end]
+X = data[:, args.x_units]
+Ys = data[:, state_intensity_index_start:state_intensity_index_end]
 
-if args.comparison:
-    for i in range(len(ys[0,:])):
-        ys[:,i] = ys[:,i] / max(ys[:,i])
-else:
-    ys = ys / np.max(ys)
+def plotter(x, ys):
+    ys = normalize_individuals(ys) if args.comparison else normalize_set(ys)
+    (x, ys) = filter_range(x, ys, args.range)
+
+    sns.set()
+    sns.set_style("white")
+    sns.set_style("ticks")
+
+    fig, ax = plt.subplots()
+    for i in range(args.number_states):
+        y = ys[:, i]
+        label = args.labels[i]
+        ax.plot(x, y, label=label)
+
+    xlabel = "Energy (eV)" if args.x_units == 0 else "Wavelength (nm)"
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(args.ylabel)
+    ax.legend(loc=1)
+    ax.axes.get_yaxis().set_ticks([])
+    ax.set_ylim((0, 1.5))
+    ax.text(-0.05, 0.95, args.letter, transform=ax.transAxes,
+            fontsize=17, fontweight='bold', va='top')
+    fig.savefig(args.outputfile)
+    plt.show()
+
+
+def normalize_individuals(ys):
+    '''
+    All spectra maximum values will equal
+    '''
+    return np.divide(ys, ys.max(0))
+
+def normalize_set(ys):
+    '''
+    Spectra will keep relative magnitudes
+    '''
+    return np.divide(ys, np.max(ys))
+
 
 def filter_range(xin, yin, my_range):
     y_fitered = []
@@ -46,27 +82,7 @@ def filter_range(xin, yin, my_range):
             x_filtered.append(x_temp)
     return (np.array(x_filtered), np.array(y_fitered))
 
-(x, ys) = filter_range(x, ys, args.range)
 
-for i in range(args.number_states):
-    y = ys[:, i]
-    color = color_code[i]
-    # label = 'S' + str(i+1)
-    label = args.labels[i]
-    plt.plot(x, y, color=color, label=label)
+plotter(X, Ys)
 
-if args.x_units == 0:
-    plt.xlabel('Energy, eV')
-else:
-    plt.xlabel('Wavelength, nm')
-
-ylabel = 'Intensity'
-
-plt.ylabel(ylabel)
-plt.title(args.title)
-plt.legend(loc=2)
-plt.yticks([])
-plt.ylim((0,2))
-plt.savefig(args.inputfile[:-4] + '.png')
-plt.show()
 
