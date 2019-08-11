ed940f353f4cb8b963b6271ec5f2115f347c2dca
Dustin Tracy <dtracy.uf@gmail.com>
2019-08-11 12:01:51 -0400

    cleaned up combination routine printing

--- pynasqm/trajectories.py
+++ pynasqm/trajectories.py
@@ -256,16 +256,12 @@ class Trajectories(ABC):
 
 def combine_trajectories(suffix, n_trajs, n_runs):
     prmtop = "m1.prmtop"
+    print ("-------- Combining Trajectories --------")
     for traj_id in range(1, n_trajs+1):
+        combined = "combined" if iscombined(suffix, traj_id) else "uncombined"
+        completed = "completed" if iscompleted(suffix, traj_id, n_runs) else "uncompleted"
+        print ("Traj {} {} while previously {}".format(traj_id, completed, combined))
         if iscompleted(suffix, traj_id, n_runs):
-            print("traj {} is completed".format(traj_id))
-        else:
-            print("traj {} is not completed".format(traj_id))
-        if iscombined(suffix, traj_id):
-            print("traj {} is already combined".format(traj_id))
-        else:
-            print("traj {} is not combined".format(traj_id))
-        if iscompleted(suffix, traj_id, n_runs) and not iscombined(suffix, traj_id):
             trajs = ["{}/traj_{}/restart_{}/nasqm_{}_t{}_r{}.nc".format(suffix, traj_id, restart,
                                                                         suffix, traj_id, restart)
                      for restart in range(n_runs)]
