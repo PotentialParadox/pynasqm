6440cafbecac60914709f48ee88c282ff5cabfaf
Dustin Tracy <dtracy.uf@gmail.com>
2020-02-11 12:15:54 -0500

    fluorescence now uses qmground for inputs

--- pynasqm/fluorescencetrajectories.py
+++ pynasqm/fluorescencetrajectories.py
@@ -16,7 +16,7 @@ class FluTrajectories(Trajectories):
         self._number_trajectories = user_input.n_snapshots_ex
         self._child_root = 'nasqm_flu_'
         self._job_suffix = 'flu'
-        self._parent_restart_root = 'nasqm_abs_'
+        self._parent_restart_root = 'nasqm_qmground_'
         self._amber_restart = True
 
     def _restart_name(self, index):
@@ -43,10 +43,10 @@ class FluTrajectories(Trajectories):
         input_ceon.set_istully(user_input.is_tully, user_input.qsteps)
 
     @staticmethod
-    def test_for_abs():
-        if not os.path.isdir("abs"):
-            raise AssertionError("abs directory not found.\n"\
-                                 "Did you run the ground state?\n")
+    def test_for_qmground():
+        if not os.path.isdir("qmground"):
+            raise AssertionError("qmground directory not found.\n"\
+                                 "Did you run the QM ground-state trajectories?\n")
 
     def isrestarting(self):
         return self._user_input.restart_attempt < self._user_input.n_exc_runs - 1
@@ -60,17 +60,17 @@ class FluTrajectories(Trajectories):
             self.copy_nmr_from_abs()
 
     def copy_nmr_from_abs(self):
-        source_files = ["abs/traj_{}/nmr/rst_{}.dist".format(t, t) for t in self.traj_indexes()]
+        source_files = ["qmground/traj_{}/nmr/rst_{}.dist".format(t, t) for t in self.traj_indexes()]
         output_files = ["flu/traj_{}/nmr/rst_{}.dist".format(t, t) for t in self.traj_indexes()]
         copy_files(source_files, output_files)
-        source_files = ["abs/traj_{}/nmr/closest_{}.txt".format(t, t) for t in self.traj_indexes()]
+        source_files = ["qmground/traj_{}/nmr/closest_{}.txt".format(t, t) for t in self.traj_indexes()]
         output_files = ["flu/traj_{}/nmr/closest_{}.txt".format(t, t) for t in self.traj_indexes()]
         copy_files(source_files, output_files)
 
     def copy_restarts_from_abs(self, override):
-        self.test_for_abs()
+        self.test_for_qmground()
         r = self._user_input.n_abs_runs - 1
-        source_files = ["abs/traj_{}/restart_{}/snap_for_abs_t{}_r{}.rst".format(t, r, t, r+1)
+        source_files = ["qmground/traj_{}/restart_{}/snap_for_qmground_t{}_r{}.rst".format(t, r, t, r+1)
                         for t in self.traj_indexes()]
         output_files = ["{1}/traj_{0}/restart_0/snap_for_{1}_t{0}_r0.rst".format(t, self._job_suffix)
                         for t in self.traj_indexes()]
