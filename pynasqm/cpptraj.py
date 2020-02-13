53b5a41e3cebbed485395ac2e38315ce7bbd1e0a
Dustin Tracy <dtracy.uf@gmail.com>
2020-02-13 13:05:40 -0500

    update cpptraj to handle a diffrent start, should really be using pytraj here

--- pynasqm/cpptraj.py
+++ pynasqm/cpptraj.py
@@ -5,21 +5,21 @@ import os
 from pynasqm.utils import copy_file
 import subprocess
 
-def create_restarts(amber_inputfile, output, step=None, override=None):
+def create_restarts(amber_inputfile, output, start=1, last="last", step=None, override=None):
     '''
     Create amber restart files using cpptraj
     '''
     if os.path.isfile(amber_inputfile):
         ctc = 'parm m1.prmtop\n'
         if step is not None:
-            ctc += "trajin {} 1 last {}\n".format(amber_inputfile, step)
+            ctc += f"trajin {amber_inputfile} {start} {last} {step}\n"
         else:
-            ctc += "trajin {}\n".format(amber_inputfile)
-        ctc += 'center :1\n' \
-            'trajout {} restart\n' \
-            'image\n' \
-            'run\n' \
-            'quit'.format(output)
+            ctc += f"trajin {amber_inputfile}\n"
+        ctc += f"center :1\n" \
+            f"trajout {output} restart\n" \
+            f"image\n" \
+            f"run\n" \
+            f"quit"
         open('convert_to_crd.in', 'w').write(ctc)
         print("Creating restarts from", amber_inputfile)
         subprocess.run(['cpptraj', '-i', 'convert_to_crd.in', '-o', 'convert_to_crd.out'])
