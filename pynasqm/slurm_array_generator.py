0c092a6e5fa6b84d0fb64d8917d3079fd13d6007
Dustin Tracy <dtracy.uf@gmail.com>
2020-02-26 10:03:18 -0500

    corrected label for pulse_pump

--- pynasqm/slurm_array_generator.py
+++ pynasqm/slurm_array_generator.py
@@ -7,13 +7,14 @@ def generate_slurm_array(user_input, job_suffix):
     return array_from_ntrajs(user_input, job_suffix)
 
 def get_number_trajectories(user_input, job_suffix):
+    print(job_suffix)
     if job_suffix == "qmground":
         return user_input.n_snapshots_qmground
     if job_suffix == "qmexcited":
         return user_input.n_snapshots_ex
     if job_suffix == "abs":
         return user_input.n_snapshots_qmground
-    if job_suffix == "pulsepump":
+    if job_suffix == "pulse_pump":
         return user_input.n_snapshots_ex
 
 def should_read_from_file(user_input, job_suffix):
