62e532819b4928b380bf10b73dc8334080d968a3
Dustin Tracy <dtracy.uf@gmail.com>
2020-02-26 06:50:02 -0500

    added read from file to slurm_array_generator

--- pynasqm/slurm_array_generator.py
+++ pynasqm/slurm_array_generator.py
@@ -1,6 +1,9 @@
+import sys
+from os import path
+
 def generate_slurm_array(user_input, job_suffix):
     if should_read_from_file(user_input, job_suffix):
-        pass
+        return read_array_from_file(user_input, job_suffix)
     return array_from_ntrajs(user_input, job_suffix)
 
 def get_number_trajectories(user_input, job_suffix):
@@ -14,11 +17,25 @@ def get_number_trajectories(user_input, job_suffix):
         return user_input.n_snapshots_ex
 
 def should_read_from_file(user_input, job_suffix):
-    if job_suffix == "qmground":
-        return user_input.qmground_traj_index_file == ""
-    if job_suffix == "qmexcited":
-        return user_input.qmexcited_traj_index_file == ""
+    filename =  get_filename(user_input, job_suffix)
+    if filename == "":
+        return False
+    if path.exists(filename) == False:
+        sys.exit(f"User requested to use trajectory file {filename}. But {filename} does not exist.")
+    return True
 
 def array_from_ntrajs(user_input, job_suffix):
     ntrajs = get_number_trajectories(user_input, job_suffix)
     return f"1-{ntrajs}"
+
+def read_array_from_file(user_input, job_suffix):
+    filename = get_filename(user_input, job_suffix)
+    return (open(filename, 'r').read()).strip()
+
+def get_filename(user_input, job_suffix):
+    if job_suffix == "qmground":
+        return user_input.qmground_traj_index_file
+    if job_suffix == "qmexcited":
+        return user_input.qmexcited_traj_index_file
+    return ""
+
