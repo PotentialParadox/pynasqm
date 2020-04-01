8139bd7a38f77a5f2c1c56c782b3a16cf38039d4
Dustin Tracy <dtracy.uf@gmail.com>
2020-04-01 17:00:02 -0400

    extract amber restart files and get amber coordinate files to separa

---
+++ pynasqm/trajectories/amber_restart_files.py
@@ -0,0 +1,34 @@
+from functools import singledispatch
+from pynasqm.trajectories.qmground import QmGround
+from pynasqm.trajectories.qmexcited import QmExcited
+from pynasqm.trajectories.fluorescence import Fluorescence
+from pynasqm.trajectories.absorption import Absorption
+
+@singledispatch
+def amber_pc_restart_files(traj_data, restart_attempt):
+    raise NotImplementedError(f"traj_data type not supported by amber_pc_restart_files\n"\
+                              f"{traj_data}")
+@amber_pc_restart_files.register(QmGround)
+@amber_pc_restart_files.register(QmExcited)
+def _(traj_data, restart_attempt):
+    return ["snap_for_{}_t{}_r{}.rst".format(traj_data.job_suffix, i, restart_attempt+1)
+            for i in traj_indices(traj_data)]
+@amber_pc_restart_files.register(Absorption)
+@amber_pc_restart_files.register(Fluorescence)
+def _(traj_data, restart_attempt):
+    [f"snap_{snap_id}_for_{traj_data.job_suffix}_t{traj}_back.rst"
+     for traj in traj_indices(traj_data)
+     for snap_id in snap_indices(traj_data)]
+
+@singledispatch
+def amber_hpc_restart_files(traj_data, restart_attempt):
+    raise NotImplementedError(f"traj_data type not supported by amber_hpc_restart_files\n"\
+                              f"{traj_data}")
+@amber_hpc_restart_files.register(QmGround)
+@amber_hpc_restart_files.register(QmExcited)
+def _(traj_data, restart_attempt):
+    return ["snap_for_{}_t${{ID}}_r{}.rst".format(traj_data.job_suffix, restart_attempt+1)]
+@amber_hpc_restart_files.register(Absorption)
+@amber_hpc_restart_files.register(Fluorescence)
+def _(traj_data, restart_attempt):
+    return [f"snap_${{i}}_for_fluorescence_t${{ID}}_back.rst"]
---
+++ pynasqm/trajectories/getambercoordinatefiles.py
@@ -0,0 +1,46 @@
+from functools import singledispatch
+from pynasqm.trajectories.qmground import QmGround
+from pynasqm.trajectories.qmexcited import QmExcited
+from pynasqm.trajectories.fluorescence import Fluorescence
+from pynasqm.trajectories.absorption import Absorption
+from pynasqm.trajectories.utils import traj_indices, snap_indices
+
+def get_coordinate_files(traj_data):
+    if traj_data.user_input.is_hpc:
+        return hpc_coordinate_files(traj_data)
+    return pc_coordinate_files(traj_data)
+
+
+@singledispatch
+def hpc_coordinate_files(traj_data):
+    raise NotImplementedError(f"traj_data type not supported by getambercoordinatefiles\n"\
+                              f"{traj_data}")
+
+@hpc_coordinate_files.register(QmGround)
+@hpc_coordinate_files.register(QmExcited)
+def _(traj_data):
+    return ["snap_for_{}_t${{ID}}_r{}.rst".format(traj_data.job_suffix,
+                                                  traj_data.user_input.restart_attempt)]
+
+@hpc_coordinate_files.register(Absorption)
+@hpc_coordinate_files.register(Fluorescence)
+def _(traj_data):
+    return [f"snap_${{i}}_for_{traj_data.job_suffix}_t${{ID}}.rst"]
+
+@singledispatch
+def pc_coordinate_files(traj_data):
+    raise NotImplementedError(f"traj_data type not supported by getambercoordinatefiles\n"\
+                              f"{traj_data}")
+
+@pc_coordinate_files.register(QmGround)
+@pc_coordinate_files.register(QmExcited)
+def _(traj_data):
+    return ["snap_for_{}_t{}_r{}.rst".format(traj_data.job_suffix, traj, traj_data.user_input.restart_attempt)
+            for traj in traj_indices(traj_data)]
+
+@pc_coordinate_files.register(QmGround)
+@pc_coordinate_files.register(QmExcited)
+def _(traj_data):
+    return [f"snap_{snap_id}_for_{traj_data.job_suffix}_t{traj}.rst"
+            for traj in self.traj_indices()
+            for snap_id in self.snap_indices()]
