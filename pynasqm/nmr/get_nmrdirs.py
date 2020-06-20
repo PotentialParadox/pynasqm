3cc72c2cdc0606be2d9d3890ea8615a593aa6697
Dustin Tracy <dtracy.uf@gmail.com>
2020-06-20 09:36:13 -0400

    impliments get_nmrdir for qmexcited

--- pynasqm/nmr/get_nmrdirs.py
+++ pynasqm/nmr/get_nmrdirs.py
@@ -1,10 +1,13 @@
 from functools import singledispatch
 from pynasqm.trajectories.qmground import QmGround
+from pynasqm.trajectories.qmexcited import QmExcited
 
 @singledispatch
 def get_nmrdirs(traj_data):
-    raise NotImplementedError("No NMR Directories given for this traj type")
+    raise NotImplementedError("No NMR Directories given for this traj type: {traj_data}")
 
 @get_nmrdirs.register(QmGround)
+@get_nmrdirs.register(QmExcited)
 def _(traj_data):
-    return ["qmground/traj_{}/nmr".format(i) for i in range(1, traj_data.number_trajectories+1)]
+    return [f"{traj_data.job_suffix}/traj_{traj}/nmr" for traj in range(1, traj_data.number_trajectories+1)]
+
